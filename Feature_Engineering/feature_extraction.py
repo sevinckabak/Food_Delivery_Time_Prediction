20d6e7c6ae77b78f99a958a488e00469b99c443f
Sevinç Kabak <sevincbaysann@gmail.com>
2024-04-29 09:40:39 +0300

    Güncelleme yapıldı

---
+++ Feature_Engineering/feature_extraction.py
@@ -0,0 +1,504 @@
+#############################################
+# Feature Extraction (Özellik Çıkarımı)
+#############################################
+from datetime import date
+
+#############################################
+# Binary Features: Flag, Bool, True-False
+#############################################
+
+df = load()
+df.head()
+
+df.isnull.any()
+
+df["NEW_CABIN_BOOL"] = df["Cabin"].notnull().astype('int')
+
+df.groupby("NEW_CABIN_BOOL").agg({"Survived": "mean"})
+
+# iki değişken arasındaki bağımlılığı test eden oran testini uygulayacağız.
+# gözlem sayısı ve başarı sayısı
+
+from statsmodels.stats.proportion import proportions_ztest
+
+test_stat, pvalue = proportions_ztest(count=[df.loc[df["NEW_CABIN_BOOL"] == 1, "Survived"].sum(),
+                                             df.loc[df["NEW_CABIN_BOOL"] == 0, "Survived"].sum()],
+
+                                      nobs=[df.loc[df["NEW_CABIN_BOOL"] == 1, "Survived"].shape[0],
+                                            df.loc[df["NEW_CABIN_BOOL"] == 0, "Survived"].shape[0]])
+
+
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+# Ho: p1 ve p2 arasında fark yoktur der. (hayatta kalma ve kalmama durumu arasında cabin değişken bilgisinin olup olmaması arasında fark yooktur)
+# bu noktada H0 reddedilir. istatistiki olarak anlamlı bir fark vardır.
+# çok değişkenli etkiyi bilmiyoruz.
+
+df.loc[((df['SibSp'] + df['Parch']) > 0), "NEW_IS_ALONE"] = "NO"
+df.loc[((df['SibSp'] + df['Parch']) == 0), "NEW_IS_ALONE"] = "YES"
+
+df.groupby("NEW_IS_ALONE").agg({"Survived": "mean"})
+
+test_stat, pvalue = proportions_ztest(count=[df.loc[df["NEW_IS_ALONE"] == "YES", "Survived"].sum(),
+                                             df.loc[df["NEW_IS_ALONE"] == "NO", "Survived"].sum()],
+
+                                      nobs=[df.loc[df["NEW_IS_ALONE"] == "YES", "Survived"].shape[0],
+                                            df.loc[df["NEW_IS_ALONE"] == "NO", "Survived"].shape[0]])
+
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+# İki oran arasında anlamlı bir fark vardır.
+
+
+#############################################
+# Text'ler Üzerinden Özellik Türetmek
+#############################################
+
+df.head()
+
+###################
+# Letter Count
+###################
+# harfleri saydırma işlemi
+
+df["NEW_NAME_COUNT"] = df["Name"].str.len()
+
+###################
+# Word Count
+###################
+
+df["NEW_NAME_WORD_COUNT"] = df["Name"].apply(lambda x: len(str(x).split(" ")))
+
+###################
+# Özel Yapıları Yakalamak
+###################
+
+df["NEW_NAME_DR"] = df["Name"].apply(lambda x: len([x for x in x.split() if x.startswith("Dr")]))
+
+df.groupby("NEW_NAME_DR").agg({"Survived": ["mean","count"]})
+
+
+###################
+# Regex ile Değişken Türetmek
+###################
+
+df.head()
+
+df['NEW_TITLE'] = df.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+
+df[["NEW_TITLE", "Survived", "Age"]].groupby(["NEW_TITLE"]).agg({"Survived": "mean", "Age": ["count", "mean"]})
+
+#############################################
+# Date Değişkenleri Üretmek
+#############################################
+import pandas as pd
+dff = pd.read_csv("Python/Feature_Engineering/Feature_Engineering/datasets/course_reviews.csv")
+dff.head()
+dff.info()
+
+dff['Timestamp'] = pd.to_datetime(dff["Timestamp"], format="%Y-%m-%d")
+
+dff = pd.DataFrame({'Timestamp': ['01-01-2023 07:45:55', '02-02-2023', '03-03-2023 12:34:56']})
+
+# Extract date part (without time)
+dff['Timestamp'] = pd.to_datetime(dff['Timestamp'].str[:10], format='%Y-%m-%d')
+
+# Drop the original 'Timestamp' column if needed
+dff = dff.drop('Date', axis=1)
+
+# year
+dff['year'] = dff['Timestamp'].dt.year
+
+# month
+dff['month'] = dff['Timestamp'].dt.month
+
+# year diff
+dff['year_diff'] = date.today().year - dff['Timestamp'].dt.year
+
+# month diff (iki tarih arasındaki ay farkı): yıl farkı + ay farkı
+dff['month_diff'] = (date.today().year - dff['Timestamp'].dt.year) * 12 + date.today().month - dff['Timestamp'].dt.month
+
+
+# day name
+dff['day_name'] = dff['Timestamp'].dt.day_name()
+
+dff.head()
+
+
+#############################################
+# Feature Interactions (Özellik Etkileşimleri)
+#############################################
+df = load()
+df.head()
+
+df["NEW_AGE_PCLASS"] = df["Age"] * df["Pclass"]
+
+df["NEW_FAMILY_SIZE"] = df["SibSp"] + df["Parch"] + 1
+
+df.loc[(df['Sex'] == 'male') & (df['Age'] <= 21), 'NEW_SEX_CAT'] = 'youngmale'
+
+df.loc[(df['Sex'] == 'male') & (df['Age'] > 21) & (df['Age'] < 50), 'NEW_SEX_CAT'] = 'maturemale'
+
+df.loc[(df['Sex'] == 'male') & (df['Age'] >= 50), 'NEW_SEX_CAT'] = 'seniormale'
+
+df.loc[(df['Sex'] == 'female') & (df['Age'] <= 21), 'NEW_SEX_CAT'] = 'youngfemale'
+
+df.loc[(df['Sex'] == 'female') & (df['Age'] > 21) & (df['Age'] < 50), 'NEW_SEX_CAT'] = 'maturefemale'
+
+df.loc[(df['Sex'] == 'female') & (df['Age'] >= 50), 'NEW_SEX_CAT'] = 'seniorfemale'
+
+df.groupby("NEW_SEX_CAT")["Survived"].mean()
+
+
+#########################################################
+# Titanic veri seti ile Uçtan Uca Uygulama
+#########################################################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+def load_application_train():
+    data = pd.read_csv("Python/Feature_Engineering/Feature_Engineering/datasets/application_train.csv")
+    return data
+
+def load():
+    data = pd.read_csv("Python/Feature_Engineering/Feature_Engineering/datasets/titanic.csv")
+    return data
+
+df = load()
+df.head()
+df.shape
+
+
+df.columns = [col.upper() for col in df.columns]
+
+df.columns = df.columns.map(str.upper)
+
+####################################
+# 1. Feature Engineering (Değişken Mühendisliği)
+####################################
+
+# Cabin bool
+df["NEW_CABIN_BOOL"] = df["CABIN"].notnull().astype('int')
+# Name count
+df["NEW_NAME_COUNT"] = df["NAME"].str.len()
+# name word count
+df["NEW_NAME_WORD_COUNT"] = df["NAME"].apply(lambda x: len(str(x).split(" ")))
+# name dr
+df["NEW_NAME_DR"] = df["NAME"].apply(lambda x: len([x for x in x.split() if x.startswith("Dr")]))
+# name title
+df['NEW_TITLE'] = df.NAME.str.extract(' ([A-Za-z]+)\.', expand=False)
+# family size
+df["NEW_FAMILY_SIZE"] = df["SIBSP"] + df["PARCH"] + 1
+# age_pclass
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+# is alone
+df.loc[((df['SIBSP'] + df['PARCH']) > 0), "NEW_IS_ALONE"] = "NO"
+df.loc[((df['SIBSP'] + df['PARCH']) == 0), "NEW_IS_ALONE"] = "YES"
+# age level
+df.loc[(df['AGE'] < 18), 'NEW_AGE_CAT'] = 'young'
+df.loc[(df['AGE'] >= 18) & (df['AGE'] < 56), 'NEW_AGE_CAT'] = 'mature'
+df.loc[(df['AGE'] >= 56), 'NEW_AGE_CAT'] = 'senior'
+# sex x age
+df.loc[(df['SEX'] == 'male') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngmale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturemale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniormale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngfemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturefemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniorfemale'
+
+df.head()
+df.shape
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if "PASSENGERID" not in col]
+
+#############################################
+# 2. Outliers (Aykırı Değerler)
+#############################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+
+
+#############################################
+# 3. Missing Values (Eksik Değerler)
+#############################################
+
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
+missing_values_table(df)
+
+df.drop("CABIN", inplace=True, axis=1)
+
+remove_cols = ["TICKET", "NAME"]
+
+df.drop(remove_cols, axis=1, inplace=True)
+
+df["AGE"] = df["AGE"].fillna(df.groupby("NEW_TITLE")["AGE"].transform("median"))
+
+df.loc[(df['AGE'] < 18), 'NEW_AGE_CAT'] = 'young'
+df.loc[(df['AGE'] >= 18) & (df['AGE'] < 56), 'NEW_AGE_CAT'] = 'mature'
+df.loc[(df['AGE'] >= 56), 'NEW_AGE_CAT'] = 'senior'
+# sex x age
+df.loc[(df['SEX'] == 'male') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngmale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturemale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniormale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngfemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturefemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniorfemale'
+
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+
+df = df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis=0)
+
+
+#############################################
+# 4. Label Encoding
+#############################################
+
+binary_cols = [col for col in df.columns if df[col].dtype not in ["int64", "int32", "float64"]
+               and df[col].nunique() == 2]
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+
+#############################################
+# 5. Rare Encoding
+#############################################
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == 'O'
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), 'Rare', temp_df[var])
+
+    return temp_df
+
+rare_analyser(df, "SURVIVED", cat_cols)
+
+df = rare_encoder(df, 0.01)
+
+df["NEW_TITLE"].value_counts()
+
+
+#############################################
+# 6. One-Hot Encoding
+#############################################
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first, dtype=int)
+    return dataframe
+
+ode_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+
+df = one_hot_encoder(df, ode_cols)
+
+df.shape
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if "PASSENGERID" not in col]
+
+rare_analyser(df, "SURVIVED", cat_cols)
+
+
+useless_cols = [col for col in df.columns if df[col].nunique() == 2 and
+                (df[col].value_counts() / len(df) < 0.01).any(axis=None)]
+
+
+# df.drop(useless_cols, axis=1, inplace=True)
+
+#############################################
+# 7. Standart Scaler
+#############################################
+
+# bu problem için standartlaştırmaya ihtiyaç olmayabilir.
+
+scaler = StandardScaler()
+
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+df[num_cols].head()
+
+df.head()
+df.shap
+
+
+#############################################
+# 8. Model
+#############################################
+
+y = df["SURVIVED"]
+X = df.drop(["PASSENGERID", "SURVIVED"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
+
+
+#############################################
+# Hiç bir işlem yapılmadan elde edilecek skor?
+#############################################
+
+dff = load()
+dff.dropna(inplace=True)
+dff = pd.get_dummies(dff, columns=["Sex", "Embarked"], drop_first=True)
+y = dff["Survived"]
+X = dff.drop(["PassengerId", "Survived", "Name", "Ticket", "Cabin"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
+
+
+# Yeni ürettiğimiz değişkenler ne alemde?
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                      ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+
+plot_importance(rf_model, X_train)
